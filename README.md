4060e8f85621f189096999bd496337386d2a2d19
Jimmy Sinn <jimmyltsinn@gmail.com>
2017-01-07 02:34:02 +0800

    Remove cgi file status checking

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 
 [Click here to the resource page. ](https://jimmyltsinn.github.io/csci4140-asg1-tutorial/)
 
-This repository hosts the tutorial resources for assignment 1 in CSCI 4140: Open-Source Software Project Development for Spring 2015 and Spring 2014. The assignments involve to CGI, Perl (Spring 2014), Python (Spring 2015) and ImageMagick. 
+This repository hosts the tutorial resources for assignment 1 in CSCI 4140: Open-Source Software Project Development for Spring 2015 and Spring 2014. The assignments involve to CGI, Perl (Spring 2014), Python (Spring 2015) and ImageMagick.
 
-The page is written in [vanilla JavaScript](http://vanilla-js.com) with Foundation.
+The page is written with jQuery and Foundation.
--- js/csci4140.js
+++ js/csci4140.js
@@ -1,37 +1,37 @@
 function getFileInfo(obj) {
-    $.get("cgi-bin/filelist.cgi?dir=" + obj.dir + "&fn=" + obj.filename, function (data, status) {
-        try {
-            ret = JSON.parse(data); 
-        } catch (err) {
-            ret = null;
-        }
-        var rowObj = $("#" + obj.id); 
+    // $.get("cgi-bin/filelist.cgi?dir=" + obj.dir + "&fn=" + obj.filename, function (data, status) {
+    //     try {
+    //         ret = JSON.parse(data);
+    //     } catch (err) {
+    //         ret = null;
+    //     }
+        var rowObj = $("#" + obj.id);
         var linkCol = $("<div></div>");
-        linkCol.addClass("text-right column small-4"); 
+        linkCol.addClass("text-right column small-4");
         var modCol = $("<div></div>");
-        modCol.addClass(""); 
-        //modCol.addClass("columns medium-4"); 
+        modCol.addClass("");
+        //modCol.addClass("columns medium-4");
         //modCol.addClass("hide-for-small-only");
         var btn = $("<a></a>");
-        btn.addClass("button radius tiny expand"); 
-        if (obj.dir != -1 && ret == null) {
-            btn.addClass("disabled"); 
-            btn.html("Unavailable");
-            modCol.html(" ");
-        } else {
+        btn.addClass("button radius tiny expand");
+        // if (obj.dir != -1 && ret == null) {
+        //     btn.addClass("disabled");
+        //     btn.html("Unavailable");
+        //     modCol.html(" ");
+        // } else {
             if (obj.dir != -1) {
-                modCol.append($("<small>Last Modified: " + ret.edittime + "</small>")); 
-                btn.html("Download"); 
-                btn.attr("href", ret.dir + "/" + obj.filename); 
+                // modCol.append($("<small>Last Modified: " + ret.edittime + "</small>"));
+                btn.html("Download");
+                btn.attr("href", obj.dir + "/" + obj.filename);
             } else {
-                btn.html("Go"); 
-                btn.attr("href", "http://" +  obj.filename); 
+                btn.html("Go");
+                btn.attr("href", "http://" +  obj.filename);
             }
-        }
-        linkCol.append(btn); 
+        // }
+        linkCol.append(btn);
         modCol.appendTo($("#"+obj.id+" div:first-child"));
-        linkCol.appendTo(rowObj); 
-    }); 
+        linkCol.appendTo(rowObj);
+    // });
 }
 
 function genSpring2015() {
@@ -44,15 +44,15 @@ function genSpring2014() {
 
 function loadFileList(jsonUrl) {
     var today = new Date();
-    var dd = today.getDate(); 
+    var dd = today.getDate();
     var mm = today.getMonth() + 1;
     today = dd + " " + (mm == 1 ? "Jan" : mm == 2 ? "Feb" : mm) + " " + today.getFullYear();
 
     $.getJSON(jsonUrl, function (data) {
-        var topObj = $('#' + data.info.code + '-' + data.info.yr); 
+        var topObj = $('#' + data.info.code + '-' + data.info.yr);
 //topObj.empty();
 
-        var headerObj = $("<h4>"+data.info.title+" <small>"+data.info.desc+"</small></h4>"); 
+        var headerObj = $("<h4>"+data.info.title+" <small>"+data.info.desc+"</small></h4>");
         var ulObj = $("<ul class=\'accordion\' data-accordion></ul>");
         ulObj.attr('id', data.info.title);
         for (var t in data) {
@@ -63,38 +63,38 @@ function loadFileList(jsonUrl) {
             id = data.info.code + '-' + data.info.yr + '-' + t;
 
             aObj = $("<a></a>");
-            aObj.attr("href", "#" + id); 
-            aObj.html(" " + data[t].desc); 
-            aObj.append(" <small>" + data[t].title + "</small>"); 
+            aObj.attr("href", "#" + id);
+            aObj.html(" " + data[t].desc);
+            aObj.append(" <small>" + data[t].title + "</small>");
             try {
-                dateObj = $("<span></span>"); 
-                dateObj.addClass("label radius"); 
-                dateObj.addClass("success"); 
-                dateObj.html("Today"); 
+                dateObj = $("<span></span>");
+                dateObj.addClass("label radius");
+                dateObj.addClass("success");
+                dateObj.html("Today");
                 if (data[t].date == today) {
                     dateObj.prependTo(aObj);
                 }
             } catch (err) { }
-            
-            liObj.append(aObj); 
+
+            liObj.append(aObj);
 
             var divObj = $("<div id=\"" + id + "\" class=\"content\"></div>");
             liObj.append(divObj);
-            liObj.appendTo(ulObj); 
-            var cnt = 0; 
+            liObj.appendTo(ulObj);
+            var cnt = 0;
             for (var f in data[t]['filelist']) {
                 addFile(divObj, data[t]['filelist'][f]);
                 cnt++;
             }
             if (cnt == 0) {
-                alertObj = $("<div data-alert></div>"); 
-                alertObj.addClass("alert-box info"); 
+                alertObj = $("<div data-alert></div>");
+                alertObj.addClass("alert-box info");
                 var tmp = Math.random() * 2;
                 if (tmp <= 1)
                     url = 'sandwich.png';
                 else
                     url = 'compiling.png';
-                alertObj.html("<img src=\'" + url + "\' height=150 />"); 
+                alertObj.html("<img src=\'" + url + "\' height=150 />");
                 alertObj.appendTo(divObj);
             }
         }
@@ -110,20 +110,20 @@ function addFile(target, file) {
     file.id = file.dir + '-' + file.id
 
     var rowObj = $("<div class=\"row\"></div>");
-    rowObj.attr("id", file.id); 
+    rowObj.attr("id", file.id);
 
     var titleObj = $("<div></div>");
     titleObj.addClass("columns small-8");
 
-    iconObj = $("<i></i>"); 
-    iconObj.addClass("fa"); 
-    iconObj.addClass("fa-" + type); 
+    iconObj = $("<i></i>");
+    iconObj.addClass("fa");
+    iconObj.addClass("fa-" + type);
     titleObj.html(file.title + " ");
-    titleObj.append(iconObj); 
+    titleObj.append(iconObj);
 
     titleObj.appendTo(rowObj);
 
-    rowObj.appendTo(target); 
+    rowObj.appendTo(target);
 
     getFileInfo(file);
 }
